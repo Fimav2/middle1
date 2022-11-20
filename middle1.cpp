da271a7cf2c24e438385a08284d9880a1e21ef44
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-20 21:49:06 +0300

    Update middle1.cpp

--- middle1.cpp
+++ middle1.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+long long my_abs(long long number){
+    if (number>=0){
+        return number;
+    }
+    else {
+        return number * -1;
+    }
+}
 
 void itc_num_print(int number){
     cout<< number;
@@ -10,15 +18,19 @@ void itc_num_print(int number){
 
 
 int itc_len_num(long long number){
+number = my_abs(number);
     int rezult;
     rezult=0;
     while (number>0){
         rezult = rezult +1;
         number = number/10;
 }
+
     return rezult;
 }
+
 int itc_sum_num(long long number){
+    number = my_abs(number);
 int symma,zifra;
 symma = 0 ;
 while (number>0){
@@ -33,6 +45,7 @@ return symma;
 
 }
 long long itc_multi_num(long long number){
+    number = my_abs(number);
 int zifra,proiz;
     proiz = 1;
 while (number>0){
@@ -45,6 +58,7 @@ return proiz;
 
 
 int itc_max_num(long long number){
+    number = my_abs(number);
     int zifra, great;
     great = 0;
     while(number>0){
